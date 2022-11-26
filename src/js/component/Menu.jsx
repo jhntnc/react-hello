e85eeb8b9a64f458e9a032f5784eda9c3a72b9cf
jhntnc <JHONATANCALDERON2104@GMAIL.COM>
2022-11-26 00:26:55 +0000

    merge

---
+++ src/js/component/Menu.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Menu(){
+    return(<div className="container">
+<nav className="navbar bg-light">
+  <div className="container-fluid">
+    <span className="navbar-brand mb-0 h1">Star Bootstrap</span>
+  </div>
+</nav>
+
+
+
+
+    </div>);
+};
+
+export default Menu;
