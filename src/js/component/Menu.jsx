f7d9c1d4361b3898231bf8bc23ad4b3622ec7cf6
jhntnc <JHONATANCALDERON2104@GMAIL.COM>
2022-11-26 14:23:28 +0000

    cambios

--- src/js/component/Menu.jsx
+++ src/js/component/Menu.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 
 function Menu(){
-    return(<div className="container">
-<nav className="navbar bg-light">
-  <div className="container-fluid">
-    <span className="navbar-brand mb-0 h1">Star Bootstrap</span>
-  </div>
-</nav>
-
-
-
-
-    </div>);
+    return(<nav className="navbar bg-light">
+    <div className="container-fluid">
+      <span className="navbar-brand mb-0 h1">Star Bootstrap</span>
+    </div>
+    
+  </nav>);
 };
 
 export default Menu;
