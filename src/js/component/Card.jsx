c566409244393992136836a1879874924815887a
jhntnc <JHONATANCALDERON2104@GMAIL.COM>
2022-11-28 22:28:07 +0000

    cambios card

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 function Card(){
-    return( <h1>
-   a
-    </h1>
-
-
-    );
+    return(<div className="card" style="width: 18rem;">
+    <a src="..." className="card-img-top" alt="...">
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" className="btn btn-primary">Go somewhere</a>
+    </div>
+    </div>);
 };
 
 export default Card;
