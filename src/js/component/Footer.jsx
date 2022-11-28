1eaa65a4d72a1498d52559f8830c892c825be964
Gabriel-Garrido <gabrielgarrido89@gmail.com>
2022-11-28 22:34:56 +0000

    correccion de class a classname en Footer

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -2,16 +2,14 @@ import React from "react"
 
 function Footer () {
     return (
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top bg-dark ">
-            <p class="col-md-4 mb-0 text-white">© 2022, 4Geeks, PT-32, Chile</p>
-            <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-            </a>
-            <ul class="nav col-md-4 justify-content-end">
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top bg-dark ">
+            <p className="col-md-4 mb-0 text-white">© 2022, 4Geeks, PT-32, Chile</p>
+            <ul className="nav col-md-4 justify-content-end">
+                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Home</a></li>
+                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Features</a></li>
+                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Pricing</a></li>
+                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
+                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">About</a></li>
             </ul>
         </footer>
     )
