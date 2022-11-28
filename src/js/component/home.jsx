47ecff2639ac18ae49311f910821cb18e5e91183
Gabriel-Garrido <gabrielgarrido89@gmail.com>
2022-11-28 22:27:14 +0000

    agrego componente Footer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 //include images into your bundle
 import Principal from "./principal.jsx";
+import Footer from "./Footer.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="container">
 			<Principal/>
+			<Footer/>
 		</div>
 	);
 };
